8febd16f16b732c0df4ea2f3485a42cc810809cd
4nanjiang <54942284+4nanjiang@users.noreply.github.com>
2024-10-12 16:12:53 +0800

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,7 +39,7 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     title: '南江',
-    subtitle: '不企图留下多少价值的个人杂记',
+    subtitle: '不加引述的言谈，不经推敲的思考',
     description: '关于自我、生活的记录与思考',
     image: {
         src: '/dante-preview.jpg',
@@ -97,17 +97,17 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: '我尝试着，在这里讲述自己的经历与感受。',
-        text: "我叫 **南江**。我无知，没有什么知识拿来讲述；我漫无目的，没有什么故事值得说道。 对于写博客，我没有材料的累积，我想，我只能研究研究自己。大多数时候，我活的太过自我，只有某些时刻会想象我死后的世界。假如我死后，人生如一部电影一样重映，我会给电影取什么名字，我将如何用一句话介绍她？",
+        text: "我叫 **南江**。我无知，没有什么知识拿来讲述；我漫无目的，没有什么故事值得说道。 对于写博客，我没有材料的累积，我想，我只能研究研究自己。大多数时候，我活的太过自我，只是某些时刻会想象我死后的世界。假如我死后，人生如一部电影一样重映，我会给电影取什么名字，我将如何用一句话介绍她？于是，我迫切地渴望能活在当下，希望补齐电影中的遗憾，让故事多些梦想、有趣与挑战。",
         image: {
             src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            alt: '电影《横道世之介》'
         },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
+//        actions: [
+//            {
+//                text: 'Get in Touch',
+//                href: '/contact'
+//            }
+//        ]
     },
     subscribe: {
         title: 'Subscribe to Dante Newsletter',
